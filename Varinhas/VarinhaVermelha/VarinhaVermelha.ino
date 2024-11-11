b01c56d6e0e3b4d1bdc19229cc56ef0d948aa203
Vinicius <99494344+ViniciusLugli@users.noreply.github.com>
2024-11-11 02:36:37 -0300

    Add files via upload

--- Varinhas/VarinhaVermelha/VarinhaVermelha.ino
+++ Varinhas/VarinhaVermelha/VarinhaVermelha.ino
@@ -3,23 +3,16 @@
 
 #define IR_SEND_PIN 3  // Pino conectado ao emissor IR
 #define botao 4
-#define cor1 5
-#define cor2 6
-#define cor3 7
+#define led 5
 
 void setup() {
   pinMode(botao, INPUT_PULLUP);
-  pinMode(cor1, OUTPUT);
-  pinMode(cor2, OUTPUT);
-  pinMode(cor3, OUTPUT);
+  pinMode(led, OUTPUT);
   IrSender.begin(IR_SEND_PIN);
 
   Serial.begin(115200);
   Serial.println("Emissor IR iniciado");
 
-  digitalWrite(cor1, HIGH);
-  digitalWrite(cor2, HIGH);
-  digitalWrite(cor3, HIGH);
 }
 
 void loop() {
@@ -33,12 +26,8 @@ void loop() {
 
     Serial.print("Letra enviada: ");
     Serial.println(comando);
-    digitalWrite(cor1, LOW);
-    digitalWrite(cor2, LOW);
-    digitalWrite(cor3, LOW);
+    digitalWrite(led, HIGH);
     delay(3000);  // Aguarda 2 segundos antes de enviar novamente
-    digitalWrite(cor1, HIGH);
-    digitalWrite(cor2, HIGH);
-    digitalWrite(cor3, HIGH);
+    digitalWrite(led, LOW);
   }
 }
